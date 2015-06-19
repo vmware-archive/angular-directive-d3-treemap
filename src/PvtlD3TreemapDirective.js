48b412587677130917a79461276f16e655a98a02
Tom Anderson <toanderson@pivotal.io>
2015-06-19 14:24:06 +0100

    Extract the node template during the compile phase so that Angular directives inside it are not evaluated until the link phase
    Signed-off-by: Ben Grohbiel <bgrohbiel@pivotal.io>

--- src/PvtlD3TreemapDirective.js
+++ src/PvtlD3TreemapDirective.js
@@ -9,8 +9,8 @@
                 scope: {
                     data: '='
                 },
-                link: function (scope, jqElement, attrs) {
-                    var element = jqElement[0];
+                compile: function (jqTemplateElement) {
+                    var element = jqTemplateElement[0];
                     if (element.children.length !== 1) {
                         throw new Error('You must specify a node template as a single child of the directive element');
                     }
@@ -18,48 +18,55 @@
                     element.removeChild(nodeTemplate);
                     var nodeTemplateHtml = nodeTemplate.outerHTML;
 
-                    var div = d3.select(element)
-                        .append("div")
-                        .style('position', 'relative');
+                    function link(scope, jqElement, attrs) {
+                        var element = jqElement[0];
+                        var div = d3.select(element)
+                            .append("div")
+                            .style('position', 'relative');
 
-                    var width = parseInt(attrs.width, 10);
-                    var height = parseInt(attrs.height, 10);
-                    var treemap = d3.layout.treemap()
-                        .size([width, height])
-                        .value(function (d) {
-                            return 1;
-                        });
+                        var width = parseInt(attrs.width, 10);
+                        var height = parseInt(attrs.height, 10);
+                        var treemap = d3.layout.treemap()
+                            .size([width, height])
+                            .value(function (d) {
+                                return 1;
+                            });
 
-                    div.datum(scope.data)
-                        .selectAll(".pvtlD3TreemapNode")
-                        .data(treemap.nodes)
-                        .enter()
-                        .append(function (d) {
-                            var nodeScope = scope.$new();
-                            angular.extend(nodeScope, d);
-                            return $compile(angular.element(nodeTemplateHtml))(nodeScope)[0];
-                        })
-                        //.attr("class", "pvtlD3TreemapNode") // need this to update data correctly
-                        .call(position);
+                        div.datum(scope.data)
+                            .selectAll(".pvtlD3TreemapNode")
+                            .data(treemap.nodes)
+                            .enter()
+                            .append(function (d) {
+                                var nodeScope = scope.$new();
+                                angular.extend(nodeScope, d);
+                                return $compile(angular.element(nodeTemplateHtml))(nodeScope)[0];
+                            })
+                            //.attr("class", "pvtlD3TreemapNode") // need this to update data correctly
+                            .call(position);
 
-                    function position() {
-                        this.style({
-                            position: 'absolute',
-                            left: function (d) {
-                                return d.x + "px";
-                            },
-                            top: function (d) {
-                                return d.y + "px";
-                            },
-                            width: function (d) {
-                                return d.dx + "px";
-                            },
-                            height: function (d) {
-                                return d.dy + "px";
-                            }
-                        });
+                        function position() {
+                            this.style({
+                                position: 'absolute',
+                                left: function (d) {
+                                    return d.x + "px";
+                                },
+                                top: function (d) {
+                                    return d.y + "px";
+                                },
+                                width: function (d) {
+                                    return d.dx + "px";
+                                },
+                                height: function (d) {
+                                    return d.dy + "px";
+                                }
+                            });
+                        }
+
                     }
 
+                    return {
+                        post: link
+                    };
                 }
             };
         });
--- test/PvtlD3TreemapDirective.spec.js
+++ test/PvtlD3TreemapDirective.spec.js
@@ -1,12 +1,53 @@
 describe('PvtlD3TreemapDirective', function () {
     beforeEach(module('pvtlD3Treemap'));
 
+    var $rootScope, $compile;
     var width, height;
     var data;
-    var pvtlD3Treemap;
-    var d3Div;
 
-    beforeEach(inject(function ($rootScope, $compile) {
+    function makeDirective(markup) {
+        var $scope = $rootScope.$new();
+        $scope.population = data;
+
+        var pvtlD3Treemap = $compile(markup)($scope)[0];
+        $rootScope.$digest();
+        return pvtlD3Treemap.children[0];
+    }
+
+    function children(parent) {
+        var collection = parent.children,
+            array = [],
+            i;
+        for (i = 0; i < collection.length; i += 1) {
+            array.push(collection[i]);
+        }
+        return array;
+    }
+
+    function styleValue(attribute) {
+        return function (el) {
+            return el.style[attribute];
+        };
+    }
+
+    function styleValuePx(attribute) {
+        return function (el) {
+            return parseInt(styleValue(attribute)(el), 10);
+        };
+    }
+
+    function tagName(el) {
+        return el.tagName;
+    }
+
+    function textContent(el) {
+        return el.textContent;
+    }
+
+    beforeEach(inject(function (_$rootScope_, _$compile_) {
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+
         width = 300;
         height = 150;
 
@@ -32,105 +73,84 @@ describe('PvtlD3TreemapDirective', function () {
                 }
             ]
         };
-
-        var $scope = $rootScope.$new();
-        $scope.population = data;
-
-        var pvtlD3TreemapMarkup =
-            '<pvtl-d3-treemap width="' + width + '" height="' + height + '" data="population">' +
-            '<pre>{{label}}</pre>' +
-            '</pvtl-d3-treemap>';
-
-        pvtlD3Treemap = $compile(pvtlD3TreemapMarkup)($scope)[0];
-        $rootScope.$digest();
-        d3Div = pvtlD3Treemap.children[0];
     }));
 
-    function children(parent) {
-        var collection = parent.children,
-            array = [],
-            i;
-        for (i = 0; i < collection.length; i += 1) {
-            array.push(collection[i]);
-        }
-        return array;
-    }
+    describe('with a valid node template', function () {
+        var d3Container;
 
-    it('creates a d3 div which is positioned relatively', function () {
-        expect(d3Div.style.position).toBe('relative');
+        beforeEach(function () {
+            var markup =
+                '<pvtl-d3-treemap width="' + width + '" height="' + height + '" data="population">' +
+                '<pre>{{label}}</pre>' +
+                '</pvtl-d3-treemap>';
+            d3Container = makeDirective(markup);
+        });
+
+        it('creates a d3 div which is positioned relatively', function () {
+            expect(d3Container.style.position).toBe('relative');
+        });
+
+        it('creates nodes for each data item', function () {
+            expect(d3Container.children.length).toBe(data.children.length + 1);
+        });
+
+        it('spreads the nodes across the specified height and width', function () {
+            var nodeElements = children(d3Container).map(styleValue('position'));
+            expect(_.uniq(nodeElements)).toEqual(['absolute']);
+
+            expect(Math.min.apply(null, children(d3Container).map(styleValuePx('top')))).toBe(0);
+            expect(Math.min.apply(null, children(d3Container).map(styleValuePx('left')))).toBe(0);
+
+            function computeRight(el) {
+                return parseInt(el.style.left, 10) + parseInt(el.style.width, 10);
+            }
+
+            expect(Math.max.apply(null, children(d3Container).map(computeRight))).toBe(width);
+
+            function computeBottom(el) {
+                return parseInt(el.style.top, 10) + parseInt(el.style.height, 10);
+            }
+
+            expect(Math.max.apply(null, children(d3Container).map(computeBottom))).toBe(height);
+        });
+
+        it('uses the content of the directive element as a template for the nodes', function () {
+            expect(_.uniq(children(d3Container).map(tagName))).toEqual(['PRE']);
+            expect(children(d3Container).map(textContent)).toEqual(['United Kingdom', 'England', 'Scotland', 'Wales', 'Northern Ireland']);
+        });
     });
 
-    it('creates nodes for each data item', function () {
-        expect(d3Div.children.length).toBe(data.children.length + 1);
-    });
-
-    function styleValue(attribute) {
-        return function (el) {
-            var style = el.style[attribute];
-            return style;
-        };
-    }
-
-    function styleValuePx(attribute) {
-        return function (el) {
-            return parseInt(styleValue(attribute)(el), 10);
-        };
-    }
-
-    it('spreads the nodes across the specified height and width', function () {
-        var nodeElements = children(d3Div).map(styleValue('position'));
-        expect(_.uniq(nodeElements)).toEqual(['absolute']);
-
-        expect(Math.min.apply(null, children(d3Div).map(styleValuePx('top')))).toBe(0);
-        expect(Math.min.apply(null, children(d3Div).map(styleValuePx('left')))).toBe(0);
-
-        function computeRight(el) {
-            return parseInt(el.style.left, 10) + parseInt(el.style.width, 10);
-        }
-
-        expect(Math.max.apply(null, children(d3Div).map(computeRight))).toBe(width);
-
-        function computeBottom(el) {
-            return parseInt(el.style.top, 10) + parseInt(el.style.height, 10);
-        }
-
-        expect(Math.max.apply(null, children(d3Div).map(computeBottom))).toBe(height);
-    });
-
-    it('uses the content of the directive element as a template for the nodes', function () {
-        function tagName(el) {
-            return el.tagName;
-        }
-
-        function textContent(el) {
-            return el.textContent;
-        }
-
-        expect(_.uniq(children(d3Div).map(tagName))).toEqual(['PRE']);
-        expect(children(d3Div).map(textContent)).toEqual(['United Kingdom', 'England', 'Scotland', 'Wales', 'Northern Ireland']);
-    });
-
-    it('raises an error if there is no node template', inject(function ($rootScope, $compile) {
-        var badMarkup =
+    it('resolves directives in the nodes but not in the node template', function () {
+        var markup =
             '<pvtl-d3-treemap width="' + width + '" height="' + height + '" data="population">' +
+            '<div><div ng-if="!children"><pre>{{label}}</pre></div></div>' +
             '</pvtl-d3-treemap>';
-        var link = $compile(badMarkup);
 
-        expect(function () {
-            return link($rootScope.$new());
-        }).toThrow(new Error('You must specify a node template as a single child of the directive element'));
-    }));
+        var d3Container = makeDirective(markup);
 
-    it('raises an error if there is a node template with more than one element', inject(function ($rootScope, $compile) {
-        var badMarkup =
-            '<pvtl-d3-treemap width="' + width + '" height="' + height + '" data="population">' +
-            '<p></p><p></p>' +
-            '</pvtl-d3-treemap>';
-        var link = $compile(badMarkup);
+        expect(children(d3Container).map(textContent)).toEqual(['', 'England', 'Scotland', 'Wales', 'Northern Ireland']);
+    });
 
-        expect(function () {
-            return link($rootScope.$new());
-        }).toThrow(new Error('You must specify a node template as a single child of the directive element'));
-    }));
+    describe('with invalid input', function () {
+        it('raises an error if there is no node template', function () {
+            var badMarkup =
+                '<pvtl-d3-treemap width="' + width + '" height="' + height + '" data="population">' +
+                '</pvtl-d3-treemap>';
 
+            expect(function () {
+                $compile(badMarkup);
+            }).toThrow(new Error('You must specify a node template as a single child of the directive element'));
+        });
+
+        it('raises an error if there is a node template with more than one element', function () {
+            var badMarkup =
+                '<pvtl-d3-treemap width="' + width + '" height="' + height + '" data="population">' +
+                '<p></p><p></p>' +
+                '</pvtl-d3-treemap>';
+
+            expect(function () {
+                $compile(badMarkup);
+            }).toThrow(new Error('You must specify a node template as a single child of the directive element'));
+        });
+    });
 });
